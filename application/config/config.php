ca5f54e52ec20b4f00b407b7784aabf1df063edd
Dea Alverina <d.verincia@gmail.com>
2016-03-13 18:59:20 +0700

    no message

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pendeta_universitas/';
+$config['base_url'] = 'http://localhost/pendetauniv_ukdw/';
 
 /*
 |--------------------------------------------------------------------------
