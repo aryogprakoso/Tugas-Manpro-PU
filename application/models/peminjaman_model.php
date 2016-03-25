dfeb6b15e0012b4a223ed652cf9750713656e801
Sagara Mahardika Sunaryo <sagara.mahardika@ti.ukdw.ac.id>
2016-03-25 20:05:08 +0700

    test

--- application/models/peminjaman_model.php
+++ application/models/peminjaman_model.php
@@ -5,28 +5,8 @@ class peminjaman_model extends CI_Model{
     function __construct() {
         parent::__construct();
     }
-    
-    public function tambahPeminjaman($data) 
-    {
-        //Query dibawah ini untuk mendapatkan dari 'idPenanggungJawab'
-        //sesuai dengan 'namaPenanggungJawab yang' ada di table 'peminjaman'
-        $this->db->select('idPenanggungJawab');
-        $this->db->where('namaPenanggungJawab', $data['penanggungJawab']);
-        $query = $this->db->get('penanggungjawab')->row();
-        
-        $insertTable = array(
-            'waktuPeminjaman' => $data['waktuModal'],
-            'ruangPeminjaman' => $data['ruang'],
-            'alatPeminjaman'  => $data['alat'],
-            'keteranganPeminjaman' => $data['keterangan'],
-            'idPenanggungJawab' => $query->idPenanggungJawab
-        );
-            
-        $this->db->insert('peminjaman', $insertTable);
-        $this->lihatPeminjaman($data['waktuSearch']);
-    }
-    
-    public function lihatPeminjaman($data)
+
+    public function lihatData($data)
     {
         $data = strtotime($data);       //$data dengan format STRING diubah menjadi TIME
         $month = date("m", $data);      //$month menampung INT dari bulan yang ada pada $data
@@ -38,42 +18,20 @@ class peminjaman_model extends CI_Model{
         
         foreach ($query->result() as $row)      //for untuk setiap row yang didapatkan dari query
         {
-            //Query dibawah ini untuk mendapatkan dari 'namaPenanggungJawab'
-            //sesuai dengan 'idPenanggungJawab yang' ada di table 'peminjaman'
-            $this->db->select('namaPenanggungJawab');
-            $this->db->where('idPenanggungJawab', $row->idPenanggungJawab);
-            $query2 = $this->db->get('penanggungjawab')->row();
-            
-            echo "<tr><td>" . $row->waktuPeminjaman . "</td>";      //echo ini digunakan untuk memunculkan waktuPeminjaman pada Tabel Peminjaman
+            echo "<tr><td>" . $row->waktuPeminjaman . "</td>";      //echo ini digunakan untuk memunculkan waktuPeminjaman pada Tabel Peminjaman    
             if($row->ruang == 0)                //echo ini digunakan untuk memunculkan ruang pada Tabel Peminjaman
                 echo "<td> Kapel Atas </td>";
             else
                 echo "<td> Kapel Bawah </td>";
             echo "<td>" . $row->alat . "</td>"; //echo ini digunakan untuk memunculkan alat pada Tabel Peminjaman
             echo "<td>" . $row->keteranganPeminjaman . "</td>"; //echo ini digunakan untuk memunculkan keteranganPeminjaman pada Tabel Peminjaman
-            echo "<td>" . $query2->namaPenanggungJawab . "</td></tr>"; //echo ini digunakan untuk memunculkan namaPenanggungJawab pada Tabel penanggungjawab
+            echo "<td>" . $row->idPenanggungJawab . "</td></tr>"; //echo ini digunakan untuk memunculkan idPenanggungJawab pada Tabel Peminjaman
         }
     }
     
-    public function editData() 
+    public function tambah_data() 
     {
         
     }
     
-    public function hapusData() 
-    {
-        
-    }
-    
-    public function lihatPenanggungJawab() 
-    {
-        //SELECT semua data yang ada pada table 'penanggungjawab'
-        $query = $this->db->get('penanggungjawab');
-        
-        foreach($query->result() as $row)
-        {
-            //echo ini digunakan untuk menampilkan semua 'namaPenanggungJawab' yang ada didalam table 'penanggungjawab'
-            echo "<option>" . $row->namaPenanggungJawab . "</option>";
-        }
-    }
 }
