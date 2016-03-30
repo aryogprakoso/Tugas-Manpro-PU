e83d444ade4612e2e8e598c415eae07f9ba32572
AgungPrasetyaDK <agung.prasetya@ti.ukdw.ac.id>
2016-03-30 13:04:02 +0700

    Update c_galeri.php

--- application/controllers/c_galeri.php
+++ application/controllers/c_galeri.php
@@ -1,12 +1,47 @@
 <?php if(! defined('BASEPATH')) exit ('No direct script access allowed');
 
 class C_galeri extends CI_Controller{
-
+    public funtion __construct(){
+        parent::__construct();
+        $this->load->helper(array('form','url'));
+        $this->load->model('m_galeri');
+    }
 
     public function index(){
      	$this->load->view('v_galeri');   
     }
 
+    public function do_upload(){
+        $keteranganGambar = $this->input->post('keteranganGambar');
+        
+        $config['file_name']=$foto;
+        $config['upload_path']= './assets/uploads';
+        $config['allowed_types']='gif|jpeg|png|jpg|bmp';
+        $config['max_size']='5000';
+        $config['max_width']='10000';
+        $config['max_height']='10000';
+        $config['remove_spaces'] = TRUE;
+        $this->load->library('upload',$config);
+        $this->upload->initialize($config);
+        
+        $id = $this->db->query("SELECT count(`idGaleri`) as `count` FROM `pu`.`galeri`" )->row_array()['count'];
+        
+        if($this->upload->do_upload('userfile')){
+                //Setting values for tabel columns
+                $data = array(
+                    'idGaleri' => $id,
+                    'keteranganGambar' => $this->input->post('keteranganGambar'),
+                    'pathGambar' => $foto
+                );
+                //Transfering data to Model
+                $this->m_galeri->form_insert($data);
+                $data['message'] = 'Gambar Berhasil Diupload';
+                redirect('/c_galeri', 'refresh');
+            }
+        else{
+
+            }
+    }
 }
 
 ?>
