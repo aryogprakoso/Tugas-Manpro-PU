d96b0c3df1cf7dbadabb8c65dc204a904a785c17
Dea Alverina <dea.alverina@ti.ukdw.ac.id>
2016-05-06 08:35:40 +0700

    no message

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -34,7 +34,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <body class="">
     <header class="">
-        <nav class="navbar navbar-fixed-top navbar-sticky navbar-css">
+        <nav class="navbar navbar-fixed-top navbar-sticky">
             <div class="container">
                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header" style="margin-right:1em">
